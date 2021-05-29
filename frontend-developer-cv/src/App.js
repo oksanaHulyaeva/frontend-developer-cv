03995241ea681265632f1e7b8d9c31a767d668e0
oksanaHulyaeva <hulyaevaoksana@gmail.com>
2021-05-30 02:46:11 +0300

    init: init project

---
+++ frontend-developer-cv/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Capture } from './components/Capture'
+
+export const App = () => {
+  return (
+    <>
+      <Capture title={'Hello'} />
+    </>
+
+  );
+};
---
+++ frontend-developer-cv/src/components/Capture.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './capture.scss'
+
+export const Capture = ({title}) => {
+  return (
+    <h3>{title}</h3>
+  )
+}
